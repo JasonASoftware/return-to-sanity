8f39e5edd3497fcda786661608c6c0ce59e370c1
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-12-08 17:21:47 -0500

    create js file with adding code

---
+++ debug.js
@@ -0,0 +1,13 @@
+//bugged function 
+// function mystery() {
+// //   var results = {
+// //     sanity: 'Hello'};
+//   return results
+// }
+
+//debugged function
+var results = {
+    sanity: 'Hello'};
+function mystery() {
+  return results
+}
